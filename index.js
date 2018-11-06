4b3e7e1b4802383455329c659dd5711702247b9a
Rose <rhaneyrrn@gmail.com>
2018-11-05 20:55:40 -0600

    Made object array in JS

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ const file = 'data.json'
 jsonfile.readFile(file)
   .then(obj => console.dir(obj))
   .catch(error => console.error(error))
+/*
+  var jData={};
+  $.getJSON('data.json', function(data) { 
+    jData=data;
+  }); 
+*/
+
+var item1 = {
+    fname: 'rafael',
+    lname: 'marques',
+    age: '19'
+}
+
+var item2 = {
+    fname: 'daniel',
+    lname: 'marques',
+    age: '19'
+}
+
+var items = [item1, item2];
+/*
+_.forEach(items => {
+    console.log(value);
+});
+*/
